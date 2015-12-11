61da151261454f50725c5d621a4432a7d97ad6c8
bakuryu101 <bakuryu101@users.noreply.github.com>
2015-12-11 14:56:56 -0400

    Update LocalAbrirArchivo.h

--- src/ios/LocalAbrirArchivo.h
+++ src/ios/LocalAbrirArchivo.h
@@ -10,24 +10,9 @@
 #import <Cordova/CDV.h>
 
 @interface LocalAbrirArchivo : CDVPlugin<UIDocumentInteractionControllerDelegate>{
-    
-    
-    
-    //variable donde esta la imagen codificada Base64
-    NSString *imagenBase64;
-    
-    //variable donde esta el Pdf codificado Base64
-    NSString *pdfBase64;
-    
-    //variable donde esta el Pdf codificado Base64
-    NSString *WordBase64;
-    
-    //variable donde esta el Pdf codificado Base64
-    NSString *ExcelBase64;
-
+  
    UIDocumentInteractionController *controller;
 }
 - (void)abriropcionesarchivo:(CDVInvokedUrlCommand*)command;
 
 @end
-
