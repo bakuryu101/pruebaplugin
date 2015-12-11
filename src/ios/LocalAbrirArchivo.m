7fb2afd4436ae3e3f39f591ed8c18ac141a22f74
bakuryu101 <bakuryu101@users.noreply.github.com>
2015-12-11 15:04:37 -0400

    Update LocalAbrirArchivo.m

--- src/ios/LocalAbrirArchivo.m
+++ src/ios/LocalAbrirArchivo.m
@@ -1,4 +1,4 @@
-//
+    //
 //  ViewController.m
 //  DecodificarBase64
 //
@@ -14,19 +14,33 @@
 - (void)abriropcionesarchivo:(CDVInvokedUrlCommand *)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-    UIAlertView *alert = [[UIAlertView alloc]
-                          initWithTitle:echo
-                          message:nil
-                          delegate:self
-                          cancelButtonTitle:@"Cancel"
-                          otherButtonTitles:@"OK", nil];
-    [alert show];
+    NSString* StringBase64 = [command.arguments objectAtIndex:0];
+//    UIAlertView *alert = [[UIAlertView alloc]
+//                          initWithTitle:echo
+//                          message:nil
+//                          delegate:self
+//                          cancelButtonTitle:@"Cancel"
+//                          otherButtonTitles:@"OK", nil];
+//    [alert show];
 
-    if (echo != nil && [echo length] > 0) {
+    if (StringBase64 != nil && [StringBase64 length] > 0) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+        
+        //Decodificando el pdf a un variable de tipo Data
+        NSData *data = [[NSData alloc]initWithBase64EncodedString:StringBase64 options:NSDataBase64DecodingIgnoreUnknownCharacters];
+        //Declaracion de una variable tipo error para que capture algun error existente
+        NSError *error = nil;
+        //Creando un archivo temporal de tipo Pdf
+        //NSString *fileName = [NSString stringWithFormat:@"%@_%@", [[NSProcessInfo processInfo] globallyUniqueString], @"file.pdf"];
+        NSString *fileName = [NSString stringWithFormat: @"Tmp_pdf.pdf"];
+        //obteniendo la direccion del archivo temporal
+        NSURL *fileURL = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:fileName]];
+        //Se escribe los datos de la informacion del pdf en el archivo temporal
+        [data writeToURL:fileURL options:NSDataWritingAtomic error:&error];
+        
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        
     }
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
