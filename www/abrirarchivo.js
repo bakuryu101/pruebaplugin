3c2377de021ae442131f4933d4f669cef00963af
bakuryu101 <consultoraimpacto@Mac-mini-de-Consultora.local>
2015-12-17 17:52:36 -0400

    a
    a

--- www/abrirarchivo.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    abriropcionesarchivo: function (base64, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "LocalAbrirArchivo", "abriropcionesarchivo", [base64]);
-    }
-};
-
