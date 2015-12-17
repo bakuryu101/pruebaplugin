5d060599a404b77bcb47d2186de78ef1d9b72f62
bakuryu101 <bakuryu101@users.noreply.github.com>
2015-12-17 17:18:37 -0400

    Update LocalAbrirArchivo.java

--- src/android/LocalAbrirArchivo.java
+++ src/android/LocalAbrirArchivo.java
@@ -7,6 +7,21 @@ import org.json.JSONObject;
 
 import android.content.Context;
 import android.widget.Toast;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class LocalAbrirArchivo extends CordovaPlugin {
     
     public LocalAbrirArchivo() {
