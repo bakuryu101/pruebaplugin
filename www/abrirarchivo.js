f84bcccf2b358f543435ed330533abd39f962cf8
bakuryu101 <bakuryu101@users.noreply.github.com>
2016-09-13 13:26:19 -0400

    Update abrirarchivo.js

--- www/abrirarchivo.js
+++ www/abrirarchivo.js
@@ -2,8 +2,8 @@ module.exports = {
     abriropcionesarchivo: function (base64, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "LocalAbrirArchivo", "abriropcionesarchivo", [base64]);
     },
-    abrirarchivo: function (base64, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "LocalAbrirArchivo", "abrirarchivo", [base64]);
+    creararchivotxt: function (base64, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "LocalAbrirArchivo", "creararchivotxt", [base64]);
     },
     creararchivo: function (base64, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "LocalAbrirArchivo", "creararchivo", [base64]);
